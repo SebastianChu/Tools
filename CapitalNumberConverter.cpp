df7e78fa691f769cab29ea823a45ae2f05ba5583
SebastianChu <707340493@qq.com>
2016-12-19 11:16:13 +0800

    Update CapitalNumberConverter.cpp
    Fix capital expression with multiple zeros & improve prevent codes

--- CapitalNumberConverter.cpp
+++ CapitalNumberConverter.cpp
@@ -13,21 +13,24 @@ std::string processInteger(std::string intStr);
 std::string processPointNumber(std::string ptrStr);
 std::string filterEmptyPosition(std::string numStr);
 bool IsDividedPosition(const std::vector<std::string> &rtnVec, int idx);
+void processZeroStr(std::string &numStr, std::string tempStr, std::string replace);
 
 std::string GetCapitalNumber(std::string orgStr)
 {
 	std::string rtnStr;
-	bool legalFlag = false;
+	bool legalFlag = true;
 	for (int j = 0; j < orgStr.size(); ++j)
 	{
 		if (orgStr[j] >= '0' && orgStr[j] <= '9'
-			|| j == 0 && orgStr[0] == '-')
+			|| j == 0 && orgStr[0] == '-'
+			|| orgStr[j] == '.')
 		{
 			legalFlag = true;
 		}
-		else if (orgStr[j] == '.')
+		else
 		{
-			legalFlag = true;
+			legalFlag = false;
+			break;
 		}
 	}
 	if (!legalFlag)
@@ -57,8 +60,12 @@ std::string GetCapitalNumber(std::string orgStr)
 		}
 		else
 		{
-			rtnStr = processInteger(orgStr.substr(0, ptrIndex));
-			rtnStr += processPointNumber(orgStr.substr(ptrIndex));
+			std::string integerStr = orgStr.substr(0, ptrIndex);
+			rtnStr = processInteger(integerStr);
+			if (integerStr.size() <= 18)
+			{
+				rtnStr += processPointNumber(orgStr.substr(ptrIndex));
+			}
 		}
 	}
 	else if (ptrIndex == -1)
@@ -75,6 +82,11 @@ std::string GetCapitalNumber(std::string orgStr)
 
 std::string processInteger(std::string intStr)
 {
+	if (intStr.size() > 18)
+	{
+		return "The ammount is too large! Please ensure the number is below 10^18!";
+	}
+
 	std::string rtnStr;
 	std::vector<std::string> rtnVec;
 	int adjustIdx = 0;
@@ -92,17 +104,29 @@ std::string processInteger(std::string intStr)
 
 		if (intStr.size() - 1 - i == 4)
 		{
+			if (lastIndex == 0)
+			{
+				rtnVec.push_back(capitalChatater[lastIndex]);
+			}
 			rtnVec.push_back(highRate[0]);	//万
 			adjustIdx += 4;
 		}
 		if (intStr.size() - 1 - i == 8)
 		{
+			if (lastIndex == 0)
+			{
+				rtnVec.push_back(capitalChatater[lastIndex]);
+			}
 			rtnVec.push_back(highRate[1]);	//亿
 			adjustIdx += 4;
 			lowRate.push_back("万");
 		}
 		if (intStr.size() - 1 - i == 13)
 		{
+			if (lastIndex == 0)
+			{
+				rtnVec.push_back(capitalChatater[lastIndex]);
+			}
 			rtnVec.push_back(highRate[2]);	//兆
 			adjustIdx += 5;
 			lowRate.push_back("亿");
@@ -151,13 +175,16 @@ std::string processPointNumber(const std::string ptrStr)
 		}
 		else
 		{
-			for (int i = 0; i < ptrStr.size(); ++i)
+			for (int i = 1; i < ptrStr.size(); ++i)
 			{
 				int capitalIdx = ptrStr[i] - '0';
 				if (i < ptrStr.size() - 1 || i == ptrStr.size() - 1 && capitalIdx != 0)
 				{
 					rtnStr += capitalChatater[capitalIdx];
-					rtnStr += ptrRate[i];
+					if (i < sizeof(ptrRate) / sizeof(ptrRate[0]))
+					{
+						rtnStr += ptrRate[i];
+					}
 				}
 			}
 		}
@@ -180,16 +207,32 @@ bool IsDividedPosition(const std::vector<std::string> &rtnVec, int idx)
 
 std::string filterEmptyPosition(std::string numStr)
 {
-	for (int i = 1; i < sizeof(highRate) / sizeof(highRate[0]); ++i)
+	if (numStr == "")
 	{
-		std::string tempStr = highRate[i] + highRate[i - 1];
-		int idx = numStr.find(tempStr);
-		if (idx >= 0)
+		numStr = "零";
+	}
+	else
+	{
+		for (int i = 0; i < sizeof(highRate) / sizeof(highRate[0]); ++i)
 		{
-			numStr = numStr.replace(numStr.find(tempStr), tempStr.length(), highRate[i] + "零");
+			processZeroStr(numStr, "零" + highRate[i - 1], "");// like "零万"
+			processZeroStr(numStr, "零零", "零");// like "零零"
+			if (i > 0)
+			{
+				processZeroStr(numStr, highRate[i] + highRate[i - 1], highRate[i]);// like "亿万"
+			}
 		}
 	}
 	return numStr += "元";
+}
+
+void processZeroStr(std::string &numStr, std::string tempStr, std::string replace)
+{
+	int idx = numStr.find(tempStr);
+	if (idx >= 0)
+	{
+		numStr = numStr.replace(numStr.find(tempStr), tempStr.length(), replace);
+	}
 }
 
 int main()
